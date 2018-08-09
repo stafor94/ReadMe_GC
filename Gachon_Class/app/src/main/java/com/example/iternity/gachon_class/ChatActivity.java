0a078df14cfdc32b10784bd88c8aa3454fa5423a
stafor94 <stafor@naver.com>
2018-08-09 13:17:21 +0900

    Add Watson's Base

---
+++ Gachon_Class/app/src/main/java/com/example/iternity/gachon_class/ChatActivity.java
@@ -0,0 +1,268 @@
+package com.example.iternity.gachon_class;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.ibm.mobilefirstplatform.clientsdk.android.analytics.api.Analytics;
+import com.ibm.mobilefirstplatform.clientsdk.android.core.api.BMSClient;
+import com.ibm.mobilefirstplatform.clientsdk.android.core.api.Response;
+import com.ibm.mobilefirstplatform.clientsdk.android.core.api.ResponseListener;
+import com.ibm.mobilefirstplatform.clientsdk.android.logger.api.Logger;
+import com.ibm.watson.developer_cloud.conversation.v1.Conversation;
+import com.ibm.watson.developer_cloud.conversation.v1.model.InputData;
+import com.ibm.watson.developer_cloud.conversation.v1.model.MessageOptions;
+import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class ChatActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ChatAdapter mAdapter;
+    private ArrayList messageArrayList;
+    private EditText inputMessage;
+    private ImageButton btnSend;
+    private com.ibm.watson.developer_cloud.conversation.v1.model.Context context = null;
+    private boolean initialRequest;
+    private Context mContext;
+    private String workspace_id;
+    private String conversation_username;
+    private String conversation_password;
+    private String analytics_APIKEY;
+    private Logger myLogger;
+
+    private String dates, times, buildings;
+
+    String test;
+    URLConnector task;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mContext = getApplicationContext();
+        conversation_username = mContext.getString(R.string.conversation_username);
+        conversation_password = mContext.getString(R.string.conversation_password);
+        workspace_id = mContext.getString(R.string.workspace_id);
+        analytics_APIKEY = mContext.getString(R.string.mobileanalytics_apikey);
+
+        // URLConnector
+        test = "http://192.168.0.10:8082/PHP_connection.php";
+        task = new URLConnector(test);
+
+        task.start();
+
+        try {
+            task.join();
+            System.out.println("waiting... for result");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        String result = task.getResult();
+
+        System.out.println(result);
+
+        //IBM Cloud Mobile Analytics
+        BMSClient.getInstance().initialize(getApplicationContext(), BMSClient.REGION_SYDNEY);
+        //Analytics is configured to record lifecycle events.
+        Analytics.init(getApplication(), "WatBot", analytics_APIKEY, false,false, Analytics.DeviceEvent.ALL);
+        //Analytics.send();
+        myLogger = Logger.getLogger("myLogger");
+        // Send recorded usage analytics to the Mobile Analytics Service
+        Analytics.send(new ResponseListener() {
+            @Override
+            public void onSuccess(Response response) {
+                // Handle Analytics send success here.
+            }
+
+            @Override
+            public void onFailure(Response response, Throwable throwable, JSONObject jsonObject) {
+                // Handle Analytics send failure here.
+            }
+        });
+
+        // Send logs to the Mobile Analytics Service
+        Logger.send(new ResponseListener() {
+            @Override
+            public void onSuccess(Response response) {
+                // Handle Logger send success here.
+            }
+
+            @Override
+            public void onFailure(Response response, Throwable throwable, JSONObject jsonObject) {
+                // Handle Logger send failure here.
+            }
+        });
+
+        inputMessage = (EditText) findViewById(R.id.message);
+        btnSend = (ImageButton) findViewById(R.id.btn_send);
+        String customFont = "Montserrat-Regular.ttf";
+        Typeface typeface = Typeface.createFromAsset(getAssets(), customFont);
+        inputMessage.setTypeface(typeface);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        messageArrayList = new ArrayList<>();
+        mAdapter = new ChatAdapter(messageArrayList);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+        this.inputMessage.setText("");
+        this.initialRequest = true;
+        sendMessage();
+
+        btnSend.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(checkInternetConnection()) {
+                    sendMessage();
+                }
+            }
+        });
+
+    };
+
+    // Sending a message to Watson Conversation Service
+    private void sendMessage() {
+
+        final String inputmessage = this.inputMessage.getText().toString().trim();
+        if(!this.initialRequest) {
+            Message inputMessage = new Message();
+            inputMessage.setMessage(inputmessage);
+            inputMessage.setId("1");
+            messageArrayList.add(inputMessage);
+            myLogger.info("Sending a message to Watson Conversation Service");
+
+        }
+        else
+        {
+            Message inputMessage = new Message();
+            inputMessage.setMessage(inputmessage);
+            inputMessage.setId("100");
+            this.initialRequest = false;
+
+        }
+
+        this.inputMessage.setText("");
+        mAdapter.notifyDataSetChanged();
+
+        Thread thread = new Thread(new Runnable(){
+            public void run() {
+                try {
+
+                    Conversation service = new Conversation(Conversation.VERSION_DATE_2017_05_26);
+                    service.setUsernameAndPassword(conversation_username, conversation_password);
+                    InputData input = new InputData.Builder(inputmessage).build();
+                    MessageOptions options = new MessageOptions.Builder(workspace_id).input(input).context(context).build();
+                    MessageResponse response = service.message(options).execute();
+
+                    //Passing Context of last conversation
+                    if(response.getContext() !=null)
+                    {
+                        //context.clear();
+                        context = response.getContext();
+
+                    }
+                    final Message outMessage=new Message();
+                    if(response!=null)
+                    {
+                        if(response.getOutput()!=null && response.getOutput().containsKey("text"))
+                        {
+
+                            ArrayList responseList = (ArrayList) response.getOutput().get("text");
+                            if(null !=responseList && responseList.size()>0){
+                                    String myMsg = (String)responseList.get(0);
+
+                                    for (int i = 1; i < responseList.size(); i++) {
+                                        myMsg = myMsg + "\n" + (String)responseList.get(i);
+                                    }
+                                    outMessage.setMessage(myMsg);
+                                    outMessage.setId("2");
+                            }
+                            if (context.get("dates") != null && context.get("times") != null && context.get("buildings") != null) {
+                                dates = (String)context.get("dates");
+                                times = (String)context.get("times");
+                                buildings = (String)context.get("buildings");
+                                Log.e("context => ", dates + " / " + times + " / " + buildings);
+                            }
+                            messageArrayList.add(outMessage);
+
+                        }
+
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                mAdapter.notifyDataSetChanged();
+                                if (mAdapter.getItemCount() > 1) {
+                                    recyclerView.getLayoutManager().smoothScrollToPosition(recyclerView, null, mAdapter.getItemCount()-1);
+
+                                }
+
+                            }
+                        });
+
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+
+    }
+
+    /**
+     * Check Internet Connection
+     * @return
+     */
+    private boolean checkInternetConnection() {
+        // get Connectivity Manager object to check connection
+        ConnectivityManager cm =
+                (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+
+        // Check for network connections
+        if (isConnected){
+            return true;
+        }
+        else {
+            Toast.makeText(this, " 인터넷이 연결되어있지 않습니다 ", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+    }
+
+    private void showError(final Exception e) {
+        runOnUiThread(new Runnable() {
+            @Override public void run() {
+                Toast.makeText(ChatActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+        });
+    }
+
+
+}
+
+
+
--- Gachon_Class/app/src/main/java/com/example/iternity/gachon_class/MainActivity.java
+++ Gachon_Class/app/src/main/java/com/example/iternity/gachon_class/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.iternity.gachon_class;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_menu_1, btn_menu_2, btn_menu_3, btn_menu_4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_menu_1 = (Button) findViewById(R.id.btnMain01);
+        btn_menu_2 = (Button) findViewById(R.id.btnMain02);
+        btn_menu_3 = (Button) findViewById(R.id.btnMain03);
+        btn_menu_4 = (Button) findViewById(R.id.btnMain04);
+
+        btn_menu_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(getApplicationContext(), ChatActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
+
+
 }
