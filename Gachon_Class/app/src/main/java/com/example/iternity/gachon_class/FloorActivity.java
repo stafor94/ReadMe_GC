66acf47f3b7c6cb194a8b13390c370bf6cd7b37f
stafor94 <stafor@naver.com>
2018-09-14 01:02:16 +0900

    강의 상태 표시

--- Gachon_Class/app/src/main/java/com/example/iternity/gachon_class/FloorActivity.java
+++ Gachon_Class/app/src/main/java/com/example/iternity/gachon_class/FloorActivity.java
@@ -31,7 +31,10 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class FloorActivity extends AppCompatActivity {
     private ListView mListView = null;
@@ -191,27 +194,135 @@ public class FloorActivity extends AppCompatActivity {
         }
     }
 
-    private void showResult(){
-        String TAG_JSON="lecture";
-        String TAG_ClassRoom = "강의실";
+    private void showResult() {
+        String TAG_JSON ="lecture";
+        String TAG_LectureRoom = "강의실";
+        String TAG_Time = "강의시간";
+
+        String DoW = getDayOfWeek();    // 현재요일
+        int CT = getCurrentTime();  // 현재시간
+        CT += 900;
+        Log.d("ReadMe", "CT = " + CT);
+        int[] mTimes;   // 시작시간과 종료시간을 담을 변수
 
         try {
             JSONObject jsonObject = new JSONObject(mJsonString);
             JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+            String subject = "";
 
-            for(int i=0; i<jsonArray.length(); i++){
+            for(int i=0; i< jsonArray.length(); i++){
                 JSONObject item = jsonArray.getJSONObject(i);
 
-                String classRoom = item.getString(TAG_ClassRoom);
-                mAdapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.icon_stop), classRoom);
-                mAdapter.notifyDataSetChanged();
+                String lectureRoom = item.getString(TAG_LectureRoom); // 강의실
+                String time = item.getString(TAG_Time); // 강의시간
+                mTimes = convertTime(time); // 강의 시작시간, 종료시간
 
-                Log.d("결과", "item: " + classRoom);
+                if (!subject.equals(lectureRoom)) { // 강의실이 등록되어있지 않으면
+                    mAdapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.icon_running), lectureRoom);
+
+                    subject = lectureRoom;
+                }
+                if (CT > mTimes[0] && CT < mTimes[1] && time.contains(DoW)) { // 현재시간이 수업시간이면
+                    if (subject.equals(lectureRoom)) {
+                        mAdapter.removeItem(mAdapter.getCount() - 1);
+                    }
+                    mAdapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.icon_stop), lectureRoom);
+                }
+
+
+                mAdapter.notifyDataSetChanged();
             }
         } catch (JSONException e) {
             Log.d("결과", "showResult : ", e);
         }
 
+    }
+
+    // 요일을 리턴해주는 메소드
+    private String getDayOfWeek() {
+        Calendar cal = Calendar.getInstance();
+
+        int nWeek = cal.get(Calendar.DAY_OF_WEEK);
+        if (nWeek == 1) return "일";
+        else if (nWeek == 2) return "월";
+        else if (nWeek == 3) return "화";
+        else if (nWeek == 4) return "수";
+        else if (nWeek == 5) return "목";
+        else if (nWeek == 6) return "금";
+        else return "토";
+    }
+
+    // 현재 시간을 int형으로 리턴해주는 메소드
+    private int getCurrentTime() {
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+        SimpleDateFormat sdfNow = new SimpleDateFormat("HH:mm");
+        String formatDate = sdfNow.format(date);
+        String[] times = formatDate.split(":");
+        int convertTime = (Integer.parseInt(times[0]) * 100) + Integer.parseInt(times[1]);  // int형 데이터 HHmm으로 환산
+
+        return convertTime;
+    }
+
+    // 수업 시작시간과 종료시간을 환산해주는 메소드
+    private int[] convertTime(String time) {
+        String[] times = time.split(" ");   // 공백으로 각 시간을 나눔
+        String[] eTimes = {times[0].substring(1), times[times.length - 1].substring(1)};    // 시작시간, 종료시간
+        int[] cTimes = {getStartTime(eTimes[0]), getEndTime(eTimes[1])};
+
+        return cTimes;
+    }
+
+    // 시작시간 환산
+    private int getStartTime(String time) {
+        switch (time) {
+            case "1": return 900;
+            case "2": return 1000;
+            case "3": return 1100;
+            case "4": return 1200;
+            case "5": return 1300;
+            case "6": return 1400;
+            case "7": return 1500;
+            case "8": return 1600;
+            case "9": return 1730;
+            case "10": return 1825;
+            case "11": return 1920;
+            case "12": return 2015;
+            case "13": return 2110;
+            case "14": return 2205;
+            case "A": return 930;
+            case "B": return 1100;
+            case "C": return 1230;
+            case "D": return 1400;
+            case "E": return 1530;
+            default: return 0;
+        }
+    }
+
+    // 종료시간 환산
+    private int getEndTime(String time) {
+        switch (time) {
+            case "1": return 1000;
+            case "2": return 1100;
+            case "3": return 1200;
+            case "4": return 1300;
+            case "5": return 1400;
+            case "6": return 1500;
+            case "7": return 1600;
+            case "8": return 1700;
+            case "9": return 1825;
+            case "10": return 1920;
+            case "11": return 2015;
+            case "12": return 2110;
+            case "13": return 2205;
+            case "14": return 2255;
+            case "A": return 1100;
+            case "B": return 1230;
+            case "C": return 1400;
+            case "D": return 1530;
+            case "E": return 1700;
+            default: return 0;
+        }
     }
 
     private class ViewHolder {
@@ -263,10 +374,6 @@ public class FloorActivity extends AppCompatActivity {
             }else{
                 holder = (ViewHolder) convertView.getTag();
             }
-//
-//            /* 'listview_custom'에 정의된 위젯에 대한 참조 획득 */
-//            ImageView iv_img = (ImageView) convertView.findViewById(R.id.iv_img) ;
-//            TextView tv_name = (TextView) convertView.findViewById(R.id.tv_name) ;
 
             /* 각 리스트에 뿌려줄 아이템을 받아오는데 mMyItem 재활용 */
             final MyItem myItem = getItem(position);
@@ -297,6 +404,10 @@ public class FloorActivity extends AppCompatActivity {
 
             /* mItems에 MyItem을 추가한다. */
             mItems.add(mItem);
+        }
+
+        public void removeItem(int index) {
+            mItems.remove(index);
         }
     }
 
