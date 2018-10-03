c72fc5d7c2baf96d69a6f041dcd6e2195315a773
stafor94 <stafor@naver.com>
2018-10-04 01:38:35 +0900

    버튼 이미지 변경

---
+++ Gachon_Class/app/src/main/java/com/example/iternity/gachon_class/ImageActivity.java
@@ -0,0 +1,29 @@
+package com.example.iternity.gachon_class;
+
+import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import uk.co.senab.photoview.PhotoViewAttacher;
+
+public class ImageActivity extends AppCompatActivity {
+    ImageView imageView;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        // 인텐트를 전달받아서 이미지를 설정
+        Intent myIntent = getIntent();
+        imageView.setImageResource(myIntent.getIntExtra("img", 0));
+
+        // 이미지뷰 핀치줌, 확대, 축소
+        PhotoViewAttacher attacher = new PhotoViewAttacher(imageView);
+    }
+}
--- Gachon_Class/app/src/main/java/com/example/iternity/gachon_class/SearchActivity.java
+++ Gachon_Class/app/src/main/java/com/example/iternity/gachon_class/SearchActivity.java
@@ -10,11 +10,16 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class SearchActivity extends AppCompatActivity implements OnClickListener {
     AlertDialog.Builder[] builder = new AlertDialog.Builder[11];
-    Button[] btnSearch = new Button[11];
+    ImageButton[] btnSearch = new ImageButton[11];
+    ImageView imgView;
+    final String[] buildings = {"가천관", "비전타워", "공과대학1", "공과대학2", "바이오나노대학",
+                                "한의과대학", "IT대학", "예술대학1", "예술대학2", "글로벌센터", "교육대학원"};
     final String[][] items = {
             {"9F", "8F", "7F","6F", "5F", "4F", "3F", "B1F", "B2F"},  //가천관
             {"6F", "5F", "4F", "3F", "2F", "B1F", "B2F"},  //비전타워
@@ -38,17 +43,20 @@ public class SearchActivity extends AppCompatActivity implements OnClickListener
     }
 
     private void init() {
-        btnSearch[0] = (Button) findViewById(R.id.btnSearch01);
-        btnSearch[1] = (Button) findViewById(R.id.btnSearch02);
-        btnSearch[2] = (Button) findViewById(R.id.btnSearch03);
-        btnSearch[3] = (Button) findViewById(R.id.btnSearch04);
-        btnSearch[4] = (Button) findViewById(R.id.btnSearch05);
-        btnSearch[5] = (Button) findViewById(R.id.btnSearch06);
-        btnSearch[6] = (Button) findViewById(R.id.btnSearch07);
-        btnSearch[7] = (Button) findViewById(R.id.btnSearch08);
-        btnSearch[8] = (Button) findViewById(R.id.btnSearch09);
-        btnSearch[9] = (Button) findViewById(R.id.btnSearch10);
-        btnSearch[10] = (Button) findViewById(R.id.btnSearch11);
+        imgView = (ImageView) findViewById(R.id.img_campus);
+        btnSearch[0] = (ImageButton) findViewById(R.id.btnSearch01);
+        btnSearch[1] = (ImageButton) findViewById(R.id.btnSearch02);
+        btnSearch[2] = (ImageButton) findViewById(R.id.btnSearch03);
+        btnSearch[3] = (ImageButton) findViewById(R.id.btnSearch04);
+        btnSearch[4] = (ImageButton) findViewById(R.id.btnSearch05);
+        btnSearch[5] = (ImageButton) findViewById(R.id.btnSearch06);
+        btnSearch[6] = (ImageButton) findViewById(R.id.btnSearch07);
+        btnSearch[7] = (ImageButton) findViewById(R.id.btnSearch08);
+        btnSearch[8] = (ImageButton) findViewById(R.id.btnSearch09);
+        btnSearch[9] = (ImageButton) findViewById(R.id.btnSearch10);
+        btnSearch[10] = (ImageButton) findViewById(R.id.btnSearch11);
+
+        imgView.setOnClickListener(this);
 
         // 다이어로그 생성 밑 설정
         for (int i = 0; i < 11; i++) {
@@ -59,7 +67,7 @@ public class SearchActivity extends AppCompatActivity implements OnClickListener
                 @Override
                 public void onClick(DialogInterface dialog, int position) {
                     Intent myIntent = new Intent(getApplicationContext(), FloorActivity.class);
-                    myIntent.putExtra("building", btnSearch[no].getText().toString());
+                    myIntent.putExtra("building", buildings[no]);
                     myIntent.putExtra("floor", items[no][position]);
                     startActivity(myIntent);
                 }
@@ -72,12 +80,22 @@ public class SearchActivity extends AppCompatActivity implements OnClickListener
 
     @Override
     public void onClick(View v) {
-        // 클릭한 버튼을 저장
-        Button b = (Button)v;
-        // 클릭한 버튼의 tag정보를 가져옴
-        int tag = Integer.parseInt(v.getTag().toString());
-        // 다이얼로그의 제목 지정
-        builder[tag].setTitle(b.getText().toString());
-        builder[tag].show();
+        if (v.getId() == R.id.img_campus) { // 상단 클릭시 줌인/줌아웃 액티비티
+            Intent intent = new Intent(getApplicationContext(), ImageActivity.class);
+            intent.putExtra("img", R.drawable.image_campus2);
+            startActivity(intent);
+        } else {    // 기타 버튼 클릭시
+            // 클릭한 버튼을 저장
+            ImageButton b = (ImageButton)v;
+            // 클릭한 버튼의 tag정보를 가져옴
+            int tag = Integer.parseInt(v.getTag().toString());
+            // 다이얼로그의 제목 지정
+            for (int i = 0; i < 11; i++) {
+                if (btnSearch[i].getId() == b.getId()) {
+                    builder[tag].setTitle(buildings[i]);
+                }
+            }
+            builder[tag].show();
+        }
     }
 }
