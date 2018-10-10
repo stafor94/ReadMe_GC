4b189cee539913f50e2c499833f2b670bcb2396a
stafor94 <stafor@naver.com>
2018-10-11 01:29:17 +0900

    Ver 1.2.0
    강의실 조회, 즐겨찾기 UI 수정
    인증메일 발송방식 PHP로 이관

--- Gachon_Class/app/src/main/java/com/stafor/iternity/gachon_class/LoginActivity.java
+++ Gachon_Class/app/src/main/java/com/stafor/iternity/gachon_class/LoginActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteException;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
@@ -25,9 +26,17 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Random;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener, Dialog.OnCancelListener {
+    private static String IP_ADDRESS = "stafor.cafe24.com";
+    private String mJsonString;
     LinearLayout layout_login, layout_signup;
     Button btn_login, btn_signup, btn_auth;
     CheckBox chk_auto;
@@ -39,6 +48,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     Dialog authDialog;  // dialog
 
     String email = "";  // SQLite DB에 저장된 email
+    String mEmail = ""; // 사용자가 입력한 email
 
     int authNum;    // 인증번호
     boolean auto = false;
@@ -182,7 +192,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     break;
                 }
                 if (flag_signup) {
-                    String mEmail = edit_signup.getText().toString();
+                    mEmail = edit_signup.getText().toString();
 
                     if (mEmail.equals("")) {    // 공백 입력 시
                         AlertDialog.Builder alert = new AlertDialog.Builder(LoginActivity.this);
@@ -229,30 +239,29 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     authDialog.setCanceledOnTouchOutside(false); // Dialog 바깥 부분을 선택해도 닫히지 않게 설정함.
                     authDialog.setOnCancelListener(this);   // Dialog를 닫을 때 일어날 일을 정의하기 위해 설정
 
-                    // Dialog 크기 조정
-                    WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
-                    lp.copyFrom(authDialog.getWindow().getAttributes());
-                    lp.width = 800;
-                    lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
-                    authDialog.show(); //Dialog를 나타내어 준다.
-                    Window window = authDialog.getWindow();
-                    window.setAttributes(lp);
-                    window.setBackgroundDrawable(new ColorDrawable(Color.DKGRAY));
-
-                    countDownTimer();   // 카운트 다운 시작
-
-                    GMailSender sender = new GMailSender("ojland17@gmail.com", "dhlwnskfk");
-                    authNum = new Random().nextInt(90000) + 10000; // 10001~99999
-                    try {
-                        sender.sendMail(
-                                "가천 Class 웹메일 인증",    // Subject
-                                "인증번호 : " + authNum + "\n\n위 인증번호를 앱에서 입력해주세요.",    // Body
-                                "ojland17@gmail.com",   // Sender
-                                mEmail + "@gc.gachon.ac.kr" // Receiver
-                        );
-                    } catch (Exception e) {
-                        Log.e("ReadMe", e.getMessage(), e);
-                    }
+                    new android.support.v7.app.AlertDialog.Builder(this)
+                            .setTitle("웹메일 인증")
+                            .setMessage("웹메일 인증을 진행하겠습니까?")
+                            .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    authNum = new Random().nextInt(90000) + 10000; // 10001~99999
+                                    new SendAuth().execute( "http://" + IP_ADDRESS + "/authMail.php", mEmail + "@gc.gachon.ac.kr", String.valueOf(authNum));
+                                }
+                            })
+                            .setNegativeButton("취소", null)
+                            .show();
+//                    GMailSender sender = new GMailSender("ojland17@gmail.com", "dhlwnskfk");
+//                    try {
+//                        sender.sendMail(
+//                                "가천 Class 웹메일 인증",    // Subject
+//                                "인증번호 : " + authNum + "\n\n위 인증번호를 앱에서 입력해주세요.",    // Body
+//                                "ojland17@gmail.com",   // Sender
+//                                mEmail + "@gc.gachon.ac.kr" // Receiver
+//                        );
+//                    } catch (Exception e) {
+//                        Log.e("ReadMe", e.getMessage(), e);
+//                    }
 
                 } else {
                     flag_signup = true;  // 간편가입 열림 상태
@@ -270,25 +279,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 }
 
                 if (user_input == authNum) {
-                    dbHelper_Login.insert(edit_signup.getText().toString()); // SQLite DB에 해당 이메일을 생성
-                    AlertDialog.Builder alert = new AlertDialog.Builder(LoginActivity.this);
-                    alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    });
-                    alert.setMessage("가입이 완료되었습니다!");
-                    alert.show();
-
-                    email = dbHelper_Login.getResult().split("&")[0]; // 이메일 정보 받아옴
-
-                    // 로그인 위젯을 띄우고 focus 요청
-                    flag_login = true;
-                    flag_signup = false;
-                    layout_login.setVisibility(View.VISIBLE);
-                    layout_signup.setVisibility(View.GONE);
-                    edit_login.requestFocus();
+                    new InsertMember().execute( "http://" + IP_ADDRESS + "/insertMember.php", edit_signup.getText().toString());
                 } else {
                     AlertDialog.Builder alert = new AlertDialog.Builder(LoginActivity.this);
                     alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
@@ -334,5 +325,171 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
 
         return super.onKeyDown(keyCode, event);
+    }
+
+    private class InsertMember extends AsyncTask<String, Void, Boolean> {
+        String errorString = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            super.onPostExecute(result);
+
+            if (result) {
+                dbHelper_Login.insert(edit_signup.getText().toString()); // SQLite DB에 해당 이메일을 생성
+                AlertDialog.Builder alert = new AlertDialog.Builder(LoginActivity.this);
+                alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                alert.setMessage("가입이 완료되었습니다!");
+                alert.show();
+
+                email = dbHelper_Login.getResult().split("&")[0]; // 이메일 정보 받아옴
+
+                // 로그인 위젯을 띄우고 focus 요청
+                flag_login = true;
+                flag_signup = false;
+                layout_login.setVisibility(View.VISIBLE);
+                layout_signup.setVisibility(View.GONE);
+                edit_login.setText(edit_signup.getText().toString());
+                edit_login.requestFocus();
+            } else {
+                Toast.makeText(getApplicationContext(),"서버와 연결이 원활하지 않습니다!", Toast.LENGTH_LONG).show();
+                try {
+                    countDownTimer.cancel();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            String serverURL = params[0];
+            String postParameters = "email=" + params[1];
+
+            try {
+                URL url = new URL(serverURL); // 주소가 저장된 변수를 이곳에 입력합니다.
+
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setReadTimeout(4000); //4초안에 응답이 오지 않으면 예외가 발생합니다.
+                httpURLConnection.setConnectTimeout(4000); //4초안에 연결이 안되면 예외가 발생합니다.
+                httpURLConnection.setRequestMethod("POST"); //요청 방식을 POST로 합니다.
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8")); //전송할 데이터가 저장된 변수를 이곳에 입력합니다.
+                outputStream.flush();
+                outputStream.close();
+
+                // 응답을 읽습니다.
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d("ReadMe", "response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    // 정상적인 응답 데이터
+                    inputStream = httpURLConnection.getInputStream();
+                    return true;
+                }
+                else{
+                    // 에러 발생
+                    inputStream = httpURLConnection.getErrorStream();
+                    return false;
+                }
+            } catch (Exception e) {
+                Log.d("ReadMe", "InsertMember : Error ", e);
+                errorString = e.toString();
+
+                return false;
+            }
+        }
+    }
+
+    private class SendAuth extends AsyncTask<String, Void, Boolean> {
+        ProgressDialog progressDialog;
+        String errorString = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = ProgressDialog.show(LoginActivity.this,
+                    "Please Wait", "서버에 연결하는 중 입니다.", true, false);
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            super.onPostExecute(result);
+            progressDialog.dismiss();
+
+            if (result) {
+                countDownTimer();   // 카운트 다운 시작
+                // Dialog 크기 조정
+                WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+                lp.copyFrom(authDialog.getWindow().getAttributes());
+                lp.width = 800;
+                lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+                authDialog.show(); //Dialog를 나타내어 준다.
+                Window window = authDialog.getWindow();
+                window.setAttributes(lp);
+                window.setBackgroundDrawable(new ColorDrawable(Color.DKGRAY));
+            } else {
+                Toast.makeText(getApplicationContext(),"서버와 연결이 원활하지 않습니다.", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            String serverURL = params[0];
+            String postParameters = "email=" + params[1] + "&authNum=" + params[2];;
+
+            try {
+                URL url = new URL(serverURL); // 주소가 저장된 변수를 이곳에 입력합니다.
+
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setReadTimeout(4000); //4초안에 응답이 오지 않으면 예외가 발생합니다.
+                httpURLConnection.setConnectTimeout(4000); //4초안에 연결이 안되면 예외가 발생합니다.
+                httpURLConnection.setRequestMethod("POST"); //요청 방식을 POST로 합니다.
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8")); //전송할 데이터가 저장된 변수를 이곳에 입력합니다.
+                outputStream.flush();
+                outputStream.close();
+
+                // 응답을 읽습니다.
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d("ReadMe", "response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    // 정상적인 응답 데이터
+                    inputStream = httpURLConnection.getInputStream();
+                    return true;
+                }
+                else{
+                    // 에러 발생
+                    inputStream = httpURLConnection.getErrorStream();
+                    return false;
+                }
+
+            } catch (Exception e) {
+                Log.d("ReadMe", "InsertMember : Error ", e);
+                errorString = e.toString();
+
+                return false;
+            }
+        }
     }
 }
